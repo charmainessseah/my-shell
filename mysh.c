63d8103f15b8b323c478bb11c694c91b69e2655d
Charmaine Seah <charmaineseahsy@gmail.com>
2022-03-02 19:45:28 -0600

    added parsing for redirection

--- mysh.c
+++ mysh.c
@@ -8,9 +8,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
 #define MAX 512
 #define clear() printf("\033[H\033[J")
 
+/*
+ * Returns the index of the file to redirect to in tokens array. Returns -1 if invalid
+ */
+int file_redirection(char **tokens, int numArgs) {
+    int numRedirectionOperators = 0;
+    int redirectionOperatorIndex = -1;
+    for (int i = 0; i < numArgs; i++) {
+        if (!strcmp(tokens[i], ">")) { // if they are equal
+            numRedirectionOperators++;
+            redirectionOperatorIndex = i;
+        }
+    }
+
+    int invalidRedirection = 0;
+    if (numRedirectionOperators == 1) {
+        // check that only one file is specified to the right of the redirection op
+        if (redirectionOperatorIndex == numArgs - 2) {
+            return redirectionOperatorIndex + 1;
+        } 
+        invalidRedirection = 1;
+    } else if (numRedirectionOperators > 1) {
+        invalidRedirection = 1;
+    } 
+    if (invalidRedirection) {
+        write(1, "Redirection misformatted.\n", 27);
+    }
+    return -1;
+}
 /*
  * Written by Edward
  * Some (UNTESTED!!!!!) code to parse commands and break it up
@@ -29,7 +60,7 @@
  * 	fgets() literally cannot read it (as opposed to
  * 	commands that don't exist)
  */
-void parse_command(char** tokens, char* line) {
+int parse_command(char** tokens, char* line) {
 //     char line[512];
 //     if(!fgets(line, 512, fp))		// if program forcibly terminates (like CTRL+D)
 //         return -1;			// returns -1
@@ -43,14 +74,17 @@ void parse_command(char** tokens, char* line) {
     int i = 0;
     while(token != NULL) {
         tokens[i] = strdup(token);	// adds a null-terminating char to end of each token
-	token = strtok(NULL, " \t");	// isolates next parameter
-	i++;				// increments index
+	    token = strtok(NULL, " \t");	// isolates next parameter
+	    i++;				// increments index
     }
     tokens[i] = token;			// adds final token to the end
 
 //     // I haven't tested this yet, you can probably just loop
 //     // through tokens[] and print out
-    
+
+    // ----------------------------------------------------------------------------------
+    int numArgs = i;
+    return numArgs;
 }
 
 void init_shell()
@@ -76,10 +110,8 @@ int main(int argc, char **argv) {
     FILE *fp = NULL;
     if (batchMode) {
         fp = fopen(argv[1], "r");
-        printf("batch mode \n");
     } else {
         fp = stdin;
-        printf("stdin\n");
     }
     if (fp == NULL) {
         _exit(1);
@@ -95,24 +127,25 @@ int main(int argc, char **argv) {
         ssize_t  lineLength;
         lineLength = getline(&linePtr, &lineSize, fp);
         if (linePtr == NULL || lineLength == -1) {
-            write(1, "exiting shell...", 17);
+            write(1, "exiting shell...\n", 18);
             fclose(fp);
             _exit(1);
         }
         int result = strcmp(linePtr, "exit\n");
         if (!result) {
-            write(1, "exiting shell...", 17);
+            write(1, "exiting shell...\n", 18);
             _exit(1);
         }
         if (batchMode) { // echo user command
             write(1, linePtr, lineLength);
+            // write(1, "\n", 2);
         }
         // create a new process
         int status;
         int retVal = fork();
         if (retVal == 0) {
             // child process
-            printf("I am child with pid: %d\n", getpid());
+            // printf("I am child with pid: %d\n", getpid());
             // get program name - argv[0]
             // second argument is pointer to our entire argument array?
 
@@ -128,18 +161,28 @@ int main(int argc, char **argv) {
                 linePtr[lineLength - 1] = '\0';
 
 	    char** tokens = malloc(sizeof(char*) * 512);
-	    parse_command(tokens, linePtr);
+	    int numArgs = parse_command(tokens, linePtr);
+        int indexToken = file_redirection(tokens, numArgs);
+        if (indexToken != -1) {
+            // handle redirection
+            close(1); //close stdout
+            // first check for user permissions using access()
+            open(tokens[indexToken], O_WRONLY);
+        } else {
+            continue; // invalid command - do not execute
+        }
+
 	    int ret = execv(tokens[0], tokens);
 
             printf("failed to execute %s, execv() ret val: %d\n", tokens[0], ret);
             for(int i = 0; i < 512; i++) {
                 free(tokens[i]);
-	    }
+	        }
 	    free(tokens);
 	    _exit(1); // this means execv() fails
         } else {
             // parent process
-            printf("I am parent with pid: %d\n", getpid());
+            // printf("I am parent with pid: %d\n", getpid());
             int pid = retVal;
             waitpid(pid, &status, 0);
         }
